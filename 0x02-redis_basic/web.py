617db54f14170855eb270e3132e9d7432b5639d1
Dev Palo <nathannkweto@devpalo.tech>
2024-07-19 15:05:07 +0100

    refactor code

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -6,7 +6,42 @@ import requests
 from functools import wraps
 from typing import Callable
 
+#!/usr/bin/env python3
+"""
+Caching request module
+"""
+import redis
+import requests
+from functools import wraps
+from typing import Callable
 
+
+def track_get_page(fn: Callable) -> Callable:
+    """ Decorator for get_page
+    """
+    @wraps(fn)
+    def wrapper(url: str) -> str:
+        """ Wrapper that:
+            - check whether a url's data is cached
+            - tracks how many times get_page is called
+        """
+        client = redis.Redis()
+        client.incr(f'count:{url}')
+        cached_page = client.get(f'{url}')
+        if cached_page:
+            return cached_page.decode('utf-8')
+        response = fn(url)
+        client.set(f'{url}', response, 10)
+        return response
+    return wrapper
+
+
+@track_get_page
+def get_page(url: str) -> str:
+    """ Makes a http request to a given endpoint
+    """
+    response = requests.get(url)
+    return response.text
 redis_store = redis.Redis()
 '''The module-level Redis instance.
 '''
@@ -36,3 +71,39 @@ def get_page(url: str) -> str:
     and tracking the request.
     '''
     return requests.get(url).text
+#!/usr/bin/env python3
+"""
+Caching request module
+"""
+import redis
+import requests
+from functools import wraps
+from typing import Callable
+
+
+def track_get_page(fn: Callable) -> Callable:
+    """ Decorator for get_page
+    """
+    @wraps(fn)
+    def wrapper(url: str) -> str:
+        """ Wrapper that:
+            - check whether a url's data is cached
+            - tracks how many times get_page is called
+        """
+        client = redis.Redis()
+        client.incr(f'count:{url}')
+        cached_page = client.get(f'{url}')
+        if cached_page:
+            return cached_page.decode('utf-8')
+        response = fn(url)
+        client.set(f'{url}', response, 10)
+        return response
+    return wrapper
+
+
+@track_get_page
+def get_page(url: str) -> str:
+    """ Makes a http request to a given endpoint
+    """
+    response = requests.get(url)
+    return response.text
